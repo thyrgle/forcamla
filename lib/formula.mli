f2b555af470760d0b8f6b6b24b5cc7c276c46b12
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-06-20 13:57:57 -0700

    Added more comments in mli file for docs.

--- lib/formula.mli
+++ lib/formula.mli
@@ -20,43 +20,89 @@ val (=:.) : float formula -> float -> unit
 (** Update a term by evaluating a formula *)
 val (=::) : int formula -> int formula -> unit
 
+(** Extraction of the (current) value for a term or formula. *)
+
 (** Get current value of formula. Similar to (!) for ref types. *)
 val (!) : 'a formula -> 'a
 
 (** Formula creation methods. *)
 
 (** Arithmetic for integer terms. *)
+
+(** Create a formula that is the sum of two int formula. *)
 val add_form_int : int formula -> int formula -> int formula
+
+(** Create a formula that is the difference of two int formula. *)
 val sub_form_int : int formula -> int formula -> int formula
+
+(** Create a formula that is the product of two int formula. *)
 val mul_form_int : int formula -> int formula -> int formula
+
+(** Create a formula that is the quotient of two int formula *)
 val div_form_int : int formula -> int formula -> int formula
 
 (** Shorthand arithmetic for terms. *)
+
+(** Shorthand for addition in an int formula. *)
 val (+) : int formula -> int formula -> int formula
+
+(** Shorthand for subtraction in an int formula. *)
 val (-) : int formula -> int formula -> int formula
+
+(** Shorthand for multiplication in an int formula. *)
 val ( * ) : int formula -> int formula -> int formula
+
+(** Shorthand for division in an int formula. *)
 val (/) : int formula -> int formula -> int formula
 
 (** Arithmetic for float terms. *)
+
+(** Create a formula that is the sum of two float formula. *)
 val add_form_float : float formula -> float formula -> float formula
+
+(** Create a formula that is the differece of two float formula. *)
 val sub_form_float : float formula -> float formula -> float formula
+
+(** Create a formula that is the product of two float formula. *)
 val mul_form_float : float formula -> float formula -> float formula
+
+(** Create a formula that is the quotient of two float formula. *)
 val div_form_float : float formula -> float formula -> float formula
 
-(** Shorthand arithmetic for terms. *)
+(** Shorthand floating point arithmetic for terms. *)
+
+(** Shorthand for addition in a float formula. *)
 val (+.) : float formula -> float formula -> float formula
+
+(** Shorthand for subtraction in a float formula. *)
 val (-.) : float formula -> float formula -> float formula
+
+(** Shorthand for multiplication in a float formula. *)
 val ( *. ) : float formula -> float formula -> float formula
+
+(** Shorthand for division in a float formula. *)
 val (/.) : float formula -> float formula -> float formula
 
 (** Simple predicate constructors. *)
+
+(** Create a bool formula that determines if two int formula are equal. *)
 val eq_form_int : int formula -> int formula -> bool formula
+
+(** Create a bool formula that determines if two float formula are equal. *)
 val eq_form_float : float formula -> float formula -> bool formula
 
 (** Shorthand predicate constructors. *)
+
+(** Shorthand for creating a bool formula that determines if two int formulas are equal. *)
 val (=?) : int formula -> int formula -> bool formula
+
+(** Shorthand for creating a bool formula that determines if two float formulas are equal. *)
 val (=.) : float formula -> float formula -> bool formula
 
 (** Event listener constructors *)
+
+(** Listen and execute a function when a formula changes value. *)
 val on_change : 'a formula -> (unit -> unit) -> unit
+
+(** Listen and execute when a predicate (boolean formula) becomes true *)
 val when_satisfied : bool formula -> (unit -> unit) -> unit
