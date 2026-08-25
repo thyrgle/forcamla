0e4acdeb9d50ae5a2b59302d976cc10a7c348fba
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-08-25 11:32:58 -0700

    Fixed docs to reflect the previous changes.

--- lib/formula.mli
+++ lib/formula.mli
@@ -1,15 +1,16 @@
 (** Create mathematical formula, equations, and systems of equations as well as define event listeners that check for various changes in these mathematical objects. *)
 
-(** {1 The Fundamental Types} *)
+(** {1 The Fundamental Type: [formula]} *)
 
-(** A combination of terms (similar to variables) and operations that represent a mathematical formula *)
+(** A combination of terms (that is, either a variable or constant) and operations that represent a 
+    mathematical formula *)
 type 'a formula
 
-(** {1 Term Creation} *)
+(** {1 [Val] Creation} *)
 
-(** Terms are of type [formula]. In particular, a {i term} refers to a [formula] with no binary operations.
-    There is no term type, but the distinction is made because {i only} terms can be assigned values 
-    (otherwise a [NotATermException] error is thrown!).
+(** [Val]s are of type [formula]. In particular, a {i val} refers to a [formula] with no operations. That is,
+    a Val is simply a wrapped OCaml value. Note that only [Val] typed objects can have their value changed
+    by the user (see [(=:)]), otherwise a [NotATermException] error is thrown!
 *)
 
 (** Lift basic types to term types. *)
@@ -20,9 +21,9 @@ val t : 'c -> 'c formula
 (** Update a term to a new value. (If the supplied formula is {i not} a term, an error is thrown.) *)
 val (=:) : 'd formula -> 'd -> unit
 
-(** {1 Extraction: Get the (current) value for a [formula] or [system]} *)
+(** {1 Extraction: Get the (current) value for a [formula]} *)
 
-(** Get current value of a formula. Similar to [(!)] for ref types. *)
+(** Get current value of a formula. An analog to [(!)] for ref types. *)
 val (!) : 'e formula -> 'e
 
 (** {2 Register custom operator} *)
@@ -39,7 +40,7 @@ val reg_bin : ('r -> 's -> 't) -> ('r formula -> 's formula -> 't formula)
 (** Register a binary operation that returns a bool. Allows for [when_satisfied] field. *)
 val reg_bin_bool : ('r -> 's -> bool) -> ('r formula -> 's formula -> bool formula)
 
-(** {1 Formula creation methods} *)
+(** {1 Built-in [formula] creation methods} *)
 
 (** {2 Arithmetic for [int formula]} *)
 
@@ -111,21 +112,23 @@ val (^) : string formula -> string formula -> string formula
 
 (** {2 [bool formula] operations} (Shorthand below!) *)
 
+(** {3 Conjugation (and) and disjunction (or)} *)
+
 (** Logical and of two [bool formula] *)
 val and_ : bool formula -> bool formula -> bool formula
 
 (** Logical or of two [bool formula] *)
 val or_ : bool formula -> bool formula -> bool formula
 
-(** {1 [system] Constructors} *)
+(** Shorthand for logical and of two [bool formula] *)
+val (&&) : bool formula -> bool formula -> bool formula
 
-(** {2 Fundamental [system] Constructors} *)
+(** Shorthand for logical or of two [bool formula] *)
+val (||) : bool formula -> bool formula -> bool formula
 
-(** Fundamental [system] types have only one equation.
-    Given two formula [f1] and [f2], fundamental system are of the form [f1 comp f2] where [comp] is a 
-    method of {i comparing} the two formula.
-    Shorthand versions are mentioned below.
-*)
+(** {3 Comparison methods for formulas *)
+
+(** Shorthand versions below! *)
 
 (** Create an equation that determines if two int formula are equal. *)
 val eq_form : 'e formula -> 'e formula -> bool formula
@@ -145,7 +148,7 @@ val lt_form : 'i formula -> 'i formula -> bool formula
 (** Create an equation that determines if two int formula are not equal. *)
 val lte_form : 'j formula -> 'j formula -> bool formula
 
-(** {2 Shorthand Fundamental [system] Constructors} *)
+(** {3 Shorthand comparison operators} *)
 
 (** Shorthand for creating a equation that determines if two int formulas are equal. *)
 val (=) : 'e formula -> 'e formula -> bool formula
@@ -165,19 +168,7 @@ val (<) : 'i formula -> 'i formula -> bool formula
 (** Shorthand for creating a equation that determines if for two int formulas LHS <= RHS. *)
 val (<=) : 'j formula -> 'j formula -> bool formula
 
-(** {2 Combine [system] types} *)
-
-(* Connect equations via and or or. Shorthand mentioned later. *)
-
-(** {2 Shorthand Combine [system] types} *)
-
-(** Shorthand for anding two equations together. *)
-val (&&) : bool formula -> bool formula -> bool formula
-
-(** Shorthand for oring two equations together. *)
-val (||) : bool formula -> bool formula -> bool formula
-
-(** {1 [source] Operations} *)
+(** {1 [source] Construction and Listening} *)
 
 type source
 
@@ -191,20 +182,22 @@ val listen : source -> unit
   Refining event listeners via the [source] type with [exec_while] is mentioned in the next section! 
 *)
 
-(** {1 Event listener constructors} *)
+(** {2 Event listener creation} *)
 
 (** Listen and execute a function when a [formula] changes value. *)
 val on_change : 'o formula -> ('o -> 'o -> unit) -> unit
 
-(** Listen and execute when a [system] becomes true. *)
+(** Listen and execute when a [bool formula] becomes true. *)
 val when_satisfied : bool formula -> (unit -> unit) -> unit
 
-(** Source event listener. Suppose [s] has registered a system [eq].
+(** Source event listener. Suppose [s] has registered a [eq] with type ['a formula].
     Execute function if [listen s] is called and supply the current value of [eq].
+    Note: This will execute *regardless* of the value of [eq].
 *)
 val exec_always : source -> bool formula -> (bool -> unit) -> unit
 
-(** Source event listener. Suppose [s] has registered a system [eq].
+(** Source event listener. Suppose [s] has registered a [eq] with type [bool formula].
     Execute function if the [eq] is currently [true] and [listen s] is called.
+    Note: Unlike [exec_always] this will *only* execute when [eq] is [true].
 *)
 val exec_while : source -> bool formula -> (unit -> unit) -> unit
